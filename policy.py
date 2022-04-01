8d30d5ae0c1002ae846598147d363c1dc3271e7d
kunalkumarkundan31 <kunalkumarkundan@gmail.com>
2022-04-01 23:46:20 +0530

    Update policy.py

--- policy.py
+++ policy.py
@@ -24,7 +24,7 @@ def fetchToken(username,password):
         "password": password
     }   
     try:
-        response = requests.post(tokenUrl, headers=headers, json=payload,verify="false")
+        response = requests.post(tokenUrl, headers=headers, json=payload,sslVerify="false")
         tokenResponse = json.loads(response.content) 
         token = tokenResponse['access_token']
         print("---------------token generated successfully---------------")
