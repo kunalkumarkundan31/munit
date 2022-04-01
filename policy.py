c17435f51e8b85880f359216816a4b684a5c1a33
kunalkumarkundan31 <kunalkumarkundan@gmail.com>
2022-04-02 00:46:54 +0530

    Update policy.py

--- policy.py
+++ policy.py
@@ -32,7 +32,7 @@ def fetchToken(username,password):
     except Exception as e:
         print("token generation failed! please check your credentials")
         # print(e)
-        raise e
+        raise Exception("Only integers are allowed")
         return 0
         
 
